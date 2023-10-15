6d70b6b6ac9a1324101f8dd9ed79356596743988
Ij Israfil <israfilkhan494@gmail.com>
2023-10-15 20:18:23 +0600

    RMX1901: Don't compile SystemUITests
    Change-Id: Ib5114a12ecf2c6c3918a7d99867913946449a97e
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- device.mk
+++ device.mk
@@ -483,6 +483,9 @@ PRODUCT_SOONG_NAMESPACES += \
 # Speed profile services and wifi-service to reduce RAM and storage.
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
+# SystemUI
+EXCLUDE_SYSTEMUI_TESTS := true
+
 # Telephony
 PRODUCT_PACKAGES += \
     extphonelib \

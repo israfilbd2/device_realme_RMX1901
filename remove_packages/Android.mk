3dc1f1590de4468f4a127652426fa4377851b446
Ij Israfil <86546896+ij-israfil@users.noreply.github.com>
2023-11-04 22:54:46 +0600

    Remove more gapps

---
+++ remove_packages/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService CarrierSetup ConnMO DCMO DMService MyVerizonServices RecorderPrebuilt Maps YouTube Photos
+LOCAL_OVERRIDES_PACKAGES := Drive PrebuiltGmail arcore SoundAmplifierPrebuilt Safety OBDM_Permissions obdm_stub Showcase SprintDM SprintHM USCCDM
+LOCAL_OVERRIDES_PACKAGES := VZWAPNLib VzwOmaTrigger AndroidAutoStubPrebuilt GoogleTTS GoogleCamera Updater FilesPrebuilt Papers FMRadio Velvet
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
